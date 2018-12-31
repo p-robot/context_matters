882e30a0fc447d80ad6d4b3887759806760d3870
Will Probert <will@probert.co.nz>
2018-12-31 11:28:07 +0000

    Adjust setup.py to pull version number from __init__.py

--- setup.py
+++ setup.py
@@ -1,17 +1,19 @@
 from distutils.core import setup
 from Cython.Build import cythonize
-import numpy as np
+
+import context_matters
+VERSION = context_matters.__version__
 
 setup(  
     name='context_matters',
-    version="0.1",
+    version=VERSION,
     description="Reinforcement learning example for foot-and-mouth disease control",
     author="Will Probert",
-    author_email="willprobert@gmail.com",
+    author_email="william.probert@bdi.ox.ac.uk",
     license="Apache License, Version 2.0",
     url="https://github.com/p-robot/context_matters",
     requires=['numpy','cython','pandas','matplotlib'],
-    packages=["context_matters"],
+    packages=["context_matters", "context_matters.tests"],
     ext_modules = cythonize("context_matters/*.pyx"),
     classifiers = ['Development Status :: 3 - Alpha',
                    'Programming Language :: Python',
